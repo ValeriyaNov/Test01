8fcab8a9b959eb6f66872a347bdf63becbaefca4
Valeriya <wal.now@yandex.ru>
2022-11-09 21:01:38 +0300

    Изменеие

--- Programe/Program.cs
+++ Programe/Program.cs
@@ -1,32 +1,38 @@
 ﻿using static System.Console;
 Clear();
 
-string[] array=new string[4]{"fast", "fat", "cat", "s"};
-PrintArray(array);
-
+WriteLine($"Введите массив, отделяя элементы массива пробелом");
+string[]array=ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 string[] matrix=ReplacementArray(array);
+WriteLine("Новый массив:");
 PrintArray(matrix);
+
+
+
+
+
 string[] ReplacementArray(string[] array1)
 {
     int count=0;
     for (int i=0; i<array1.Length; i++)
-    if (array1[i].Length<3)
-    count++;
+        if (array1[i].Length<4)
+            count++;
+
     string[] newarray=new string[count];
     int j=0;
     for (int i=0; i<array1.Length; i++)
-    {
-            if (array1[i].Length<3)
-           { newarray[j]=array1[i];
+        if (array1[i].Length<4)
+        { 
+            newarray[j]=array1[i];
             j++;
-           }
+        }
         
-    }
+    
     return newarray;
-
 }
+
 void PrintArray(string[]prarray)
 {
-    string prArray=String.Join(",",prarray);
-WriteLine($"[{prArray}]");
+    string prArray=String.Join(", ",prarray);
+    WriteLine($"[{prArray}]");
 }
