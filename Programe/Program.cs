209b713e2f6a4e76a30c45ae2486e2ad8f0c7e65
Valeriya <wal.now@yandex.ru>
2022-11-09 20:19:20 +0300

    Добавила программу

---
+++ Programe/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+Clear();
+
+string[] array=new string[4]{"fast", "fat", "cat", "s"};
+PrintArray(array);
+
+string[] matrix=ReplacementArray(array);
+PrintArray(matrix);
+string[] ReplacementArray(string[] array1)
+{
+    int count=0;
+    for (int i=0; i<array1.Length; i++)
+    if (array1[i].Length<3)
+    count++;
+    string[] newarray=new string[count];
+    int j=0;
+    for (int i=0; i<array1.Length; i++)
+    {
+            if (array1[i].Length<3)
+           { newarray[j]=array1[i];
+            j++;
+           }
+        
+    }
+    return newarray;
+
+}
+void PrintArray(string[]prarray)
+{
+    string prArray=String.Join(",",prarray);
+WriteLine($"[{prArray}]");
+}
